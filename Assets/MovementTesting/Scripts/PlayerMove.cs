2a77e210cd96d8d2c5cdb746a4a2a5facdf350a2
Adrian McAllister <amcallister2317@student.pps.net>
2022-10-19 14:47:57 -0700

    Spider Movement

--- Assets/MovementTesting/Scripts/PlayerMove.cs
+++ Assets/MovementTesting/Scripts/PlayerMove.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveScript : MonoBehaviour
+public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public CharacterController player;
+    public float speed = 6f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+
+            player.Move(direction * speed * Time.deltaTime);
+        }
     }
 }
