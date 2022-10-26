8f66247ff7cc5ba1712afe2aeab0eb6fdf2ef575
Adrian McAllister <amcallister2317@student.pps.net>
2022-10-26 14:18:19 -0700

    Working Movement 1

--- Assets/MovementTesting/Scripts/PlayerMove.cs
+++ Assets/MovementTesting/Scripts/PlayerMove.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    public CharacterController player;
-    public float speed = 6f;
+    public GameObject player;
+    public Rigidbody rbody;
+    
+    public Transform cam;
 
+    public float speed = 6f;
+    public float turnTime = 0.1f;
+    float turnSmoothVelocity;
+    
+    void Start() 
+    {
+
+    }
+    
     void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
@@ -15,10 +26,13 @@ public class PlayerMove : MonoBehaviour
 
         if(direction.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-            player.Move(direction * speed * Time.deltaTime);
+            //Movement currently doesn't work//
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            player.transform.position = player.transform.position + (moveDir.normalized * speed * Time.deltaTime);
         }
     }
 }
