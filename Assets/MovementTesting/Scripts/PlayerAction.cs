b597b2b404fc1fbdbd61f3125e47ee0afacf650d
Adrian McAllister <amcallister2317@student.pps.net>
2022-11-09 15:26:06 -0800

    Pickup system rework (not yet working)

--- Assets/MovementTesting/Scripts/PlayerAction.cs
+++ Assets/MovementTesting/Scripts/PlayerAction.cs
@@ -8,12 +8,34 @@ public class PlayerAction : MonoBehaviour
     public GameObject holdBox;
     public bool holding;
     public GameObject heldItem;
+    public Rigidbody heldRB;
 
     void Start()
     {
         holding = false;
     }
 
+    void PickupObject() 
+    {
+        if(holding == true)
+        {
+            heldRB = heldItem.GetComponent<RigidBody>();
+            heldRB.useGravity = false;
+            heldRB.drag = 10;
+            heldRB.constraints = RigidbodyConstraints.FreezeRotation;
+
+            heldItem.transform.SetParent(holdBox); 
+        }
+
+        if(holding == false) {
+            heldRB.useGravity = true;
+            heldRB.drag = 1;
+            heldRB.constraints = RigidbodyConstraints.None;
+
+            heldItem.transform.SetParent(null); 
+            heldItem = null;
+        }
+    }
 
     void Update()
     {
@@ -31,15 +53,11 @@ public class PlayerAction : MonoBehaviour
                         Debug.Log("Object Interaction");
                         holding = true;
                         heldItem = hitCollider.gameObject;
+                        PickupObject();
                     }
                 }
             }
         }
 
-        if(holding == true)
-        {
-            heldItem.transform.position = holdBox.transform.position;
-            //heldItem.transform.SetParent(holdBox.transform);
-        }
     }
 }
